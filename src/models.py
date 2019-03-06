4faa9138bd2ab1c2eaa4d5874633d230053aa597
TheMiloAnderson <themiloanderson@gmail.com>
2019-03-06 13:49:58 -0800

    created portfolios model & form, added to search/save functionality

--- src/models.py
+++ src/models.py
@@ -15,11 +15,14 @@ class Company(db.Model):
     symbol = db.Column(db.String(16))
     exchange = db.Column(db.String(256))
     description = db.Column(db.String(1024))
+    portfolio_id = db.Column(db.ForeignKey('portfolios.id'), nullable=False)
 
-    def __repr__(self):
-        return {
-            'name': self.name,
-            'symbol': self.symbol,
-            'exchange': self.exchange,
-            'description': self.description
-        }
+
+class Portfolio(db.Model):
+    __tablename__ = 'portfolios'
+
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(128))
+    date_created = db.Column(db.DateTime, default=dt.now())
+
+    companies = db.relationship('Company', backref='portfolio', lazy=True)
