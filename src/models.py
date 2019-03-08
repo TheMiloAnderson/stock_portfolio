5a4ffa030ee14ff0c72fde35351ad5c2a4e020e9
TheMiloAnderson <themiloanderson@gmail.com>
2019-03-07 16:54:06 -0800

    company save is now constrained per portfolio, not system-wide

--- src/models.py
+++ src/models.py
@@ -12,12 +12,11 @@ migrate = Migrate(app, db)
 class Company(db.Model):
     __tablename__ = 'companies'
 
-    id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(256), index=True, unique=True)
+    name = db.Column(db.String(256), primary_key=True)
+    portfolio_id = db.Column(db.ForeignKey('portfolios.id'), primary_key=True)
     symbol = db.Column(db.String(16))
     exchange = db.Column(db.String(256))
     description = db.Column(db.String(1024))
-    portfolio_id = db.Column(db.ForeignKey('portfolios.id'), nullable=False)
 
     portfolio = db.relationship('Portfolio', back_populates='companies', lazy=True)
 
